c257290c16b8327f2ee24b7b9d77d56a238d5856
Nima Heidari <nima.heidari.n@gmail.com>
2025-08-02 14:46:58 +0330

    Feat(User,Company): Add Get All Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//USER
+
+Route::get('/users', function () {
+    return User::all();
+});
+
+Route::get('/user/create', function () {
+    return User::create([
+        'name'=>'admin',
+        'email'=>'admin@gmail.com',
+        'password'=>bcrypt('123456')
+    ]);
+});
+
+//COMPANY
+Route::get('/companies', function () {
+    return Company::all();
+});
+
+Route::get('/company/create', function () {
+    return Company::create([
+        'name'=>'My company',
+    ]);
 });
